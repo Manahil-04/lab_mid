9c65f4e10544e435feb1dbf35b6f8a4e6ea906c9
MunimKH <munimasim1213@gmail.com>
2023-11-20 15:42:44 +0500

    saved

---
+++ Calculator/cal03.py
@@ -0,0 +1,17 @@
+import math
+def add(num1,num2):
+    return num1+num2
+def sub(num1,num2):
+    return num1-num2
+def mul(num1,num2):
+    return num1*num2
+def div(num1,num2):
+    return num1/num2
+def sqrt(num1):
+    return math.sqrt(num1)
+
+print(add(5,3))
+print(sub(5,3))
+print(mul(5,3))
+print(div(5,3))
+print(sqrt(49))
